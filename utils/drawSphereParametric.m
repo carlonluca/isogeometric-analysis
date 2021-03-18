115454bebd6f3d826ed2200aa2b109abea8dd785
Luca Carlon <carlon.luca@gmail.com>
2021-03-18 12:48:20 +0100

    Added parametric sphere procedure.

---
+++ utils/drawSphereParametric.m
@@ -0,0 +1,32 @@
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <https://www.gnu.org/licenses/>.
+%
+% Copyright © 2008-2021 Luca Carlon
+
+%
+% Author: Luca Carlon
+%
+
+% Draws a sphere using a parametric equation.
+
+XIV = linspace(0, pi, 100);
+NUV = linspace(0, 2*pi, 100);
+[XI, NU] = meshgrid(XIV, NUV);
+X = sin(XI).*cos(NU);
+Y = sin(XI).*sin(NU);
+Z = cos(XI);
+mesh(X, Y, Z);
+axis equal;
+box on;
+colorbar;
+grid on;
