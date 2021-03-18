2832ccf5c0a1093220ce95448e8c98811e2eeb16
Luca Carlon <carlon.luca@gmail.com>
2021-03-18 23:39:48 +0100

    Updated header.

--- 3.1/drawEllipseImplicit.m
+++ 3.1/drawEllipseImplicit.m
@@ -1,3 +1,10 @@
+%
+% Project: Approximation and Finite Elements in Isogeometric Problems
+% Author:  Luca Carlon
+% Date:    -
+%
+% Copyright (c) 2009-2021 Luca Carlon. All rights reserved.
+%
 % This program is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
@@ -9,12 +16,7 @@
 % GNU General Public License for more details.
 %
 % You should have received a copy of the GNU General Public License
-% along with this program.  If not, see <https://www.gnu.org/licenses/>.
-%
-% Copyright © 2008-2021 Luca Carlon
-
-%
-% Author: Luca Carlon
+% along with this program. If not, see <http://www.gnu.org/licenses/>.
 %
 
 % Draws an ellipse using an implicit equation.
--- 3.1/drawSphereImplicit.m
+++ 3.1/drawSphereImplicit.m
@@ -1,4 +1,11 @@
-% his program is free software: you can redistribute it and/or modify
+%
+% Project: Approximation and Finite Elements in Isogeometric Problems
+% Author:  Luca Carlon
+% Date:    -
+%
+% Copyright (c) 2009-2021 Luca Carlon. All rights reserved.
+%
+% This program is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
 % (at your option) any later version.
@@ -9,12 +16,7 @@
 % GNU General Public License for more details.
 %
 % You should have received a copy of the GNU General Public License
-% along with this program.  If not, see <https://www.gnu.org/licenses/>.
-%
-% Copyright © 2008-2021 Luca Carlon
-
-%
-% Author: Luca Carlon
+% along with this program. If not, see <http://www.gnu.org/licenses/>.
 %
 
 % Draws a sphere using an implicit equation.
