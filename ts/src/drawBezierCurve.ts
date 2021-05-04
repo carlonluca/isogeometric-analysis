7a226509b27724194674cb35f78102e72884aae8
Luca Carlon <carlon.luca@gmail.com>
2021-05-04 13:06:47 +0200

    Implemented bezier example 1 in ts.

--- ts/src/drawBezierCurve.ts
+++ ts/src/drawBezierCurve.ts
@@ -1,7 +1,7 @@
 /**
  * Project: Approximation and Finite Elements in Isogeometric Problems
  * Author:  Luca Carlon
- * Date:    2021.05.02
+ * Date:    2021.05.04
  *
  * Copyright (c) 2021 Luca Carlon. All rights reserved.
  *
@@ -19,37 +19,30 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { bernstein } from "./bernstein.js"
 import { Point } from "./point.js"
+import { Bezier } from "./bezier.js"
 
-export class Bezier {
-    controlPoints: Point[] = []
-
-    /**
-     * Ctor.
-     * 
-     * @param controlPoints 
-     */
-    constructor(controlPoints: Point[]) {
-        this.controlPoints = controlPoints
-    }
-
-    /**
-     * Evaluates the value of the Bezier curve in the parametric space.
-     * 
-     * @param xi 
-     * @returns 
-     */
-    public evaluate(xi: number): Point {
-        let x = 0
-        let y = 0
-        let z = 0
-        let n = this.controlPoints.length
-        for (let i = 0; i < n; i++) {
-            x = x + bernstein(i, n - 1, xi)*this.controlPoints[i].x
-            y = y + bernstein(i, n - 1, xi)*this.controlPoints[i].y
-        }
-
-        return new Point(x, y)
-    }
-}
+/**
+ * Draws the bezier curve into a plot.
+ * 
+ * @param controlPoints 
+ * @param plot 
+ */
+export let drawBezierCurve = (controlPoints: Point[], plot: string) => {
+    const bezier = new Bezier(controlPoints);
+    const xiValues = math.range(0, 1, 0.001).toArray();
+    let xValues = [];
+    let yValues = [];
+    xiValues.map((xi) => {
+        let p = bezier.evaluate(xi);
+        xValues.push(p.x);
+        yValues.push(p.y);
+    });
+    const trace1 = {
+        x: xValues,
+        y: yValues,
+        type: "scatter",
+    };
+    const data = [trace1];
+    Plotly.newPlot(plot, data);
+};
--- ts/src/drawBezierCurve1.ts
+++ ts/src/drawBezierCurve1.ts
@@ -1,7 +1,7 @@
 /**
  * Project: Approximation and Finite Elements in Isogeometric Problems
  * Author:  Luca Carlon
- * Date:    2021.05.02
+ * Date:    2021.05.04
  *
  * Copyright (c) 2021 Luca Carlon. All rights reserved.
  *
@@ -19,37 +19,16 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-import { bernstein } from "./bernstein.js"
-import { Point } from "./point.js"
+import { Point } from "./point.js";
+import { drawBezierCurve } from "./drawBezierCurve.js"
 
-export class Bezier {
-    controlPoints: Point[] = []
-
-    /**
-     * Ctor.
-     * 
-     * @param controlPoints 
-     */
-    constructor(controlPoints: Point[]) {
-        this.controlPoints = controlPoints
-    }
-
-    /**
-     * Evaluates the value of the Bezier curve in the parametric space.
-     * 
-     * @param xi 
-     * @returns 
-     */
-    public evaluate(xi: number): Point {
-        let x = 0
-        let y = 0
-        let z = 0
-        let n = this.controlPoints.length
-        for (let i = 0; i < n; i++) {
-            x = x + bernstein(i, n - 1, xi)*this.controlPoints[i].x
-            y = y + bernstein(i, n - 1, xi)*this.controlPoints[i].y
-        }
-
-        return new Point(x, y)
-    }
+window.drawBezierCurve1 = (plot: string) => {
+    let controlPoints = []
+    controlPoints.push(new Point(0, 0))
+    controlPoints.push(new Point(1, 1))
+    controlPoints.push(new Point(2, 0.5))
+    controlPoints.push(new Point(3, 0.5))
+    controlPoints.push(new Point(0.5, 1.5))
+    controlPoints.push(new Point(1.5, 0))
+    drawBezierCurve(controlPoints, plot)
 }
