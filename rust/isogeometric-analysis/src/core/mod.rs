8b8d6f93237004d6af1d6e164767b0a1ae129231
Luca Carlon <carlon.luca@gmail.com>
2021-11-01 15:02:35 +0100

    Remove the size module.

---
+++ rust/isogeometric-analysis/src/core/mod.rs
@@ -0,0 +1,2 @@
+pub use self::size::Size;
+mod size;
--- rust/isogeometric-analysis/src/lib.rs
+++ rust/isogeometric-analysis/src/lib.rs
@@ -1,1 +1,13 @@
+use crate::core::Size;
 mod core;
+
+#[cfg(test)]
+mod tests {
+    use crate::core::Size;
+    fn it_works() {
+        assert_eq!(Size {
+            width: 0,
+            height: 0
+        }.is_empty(), true);
+    }
+}
