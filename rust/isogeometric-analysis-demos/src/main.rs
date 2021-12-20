7f5af9a2614b0b4ef1067b64500db78397cfc4ae
Luca Carlon <carlon.luca@gmail.com>
2021-12-20 13:55:54 +0100

    RS: draw all bernstein polynomials of degree 2.

--- rust/isogeometric-analysis-demos/src/main.rs
+++ rust/isogeometric-analysis-demos/src/main.rs
@@ -5,11 +5,20 @@ use gnuplot::{Figure, Caption, Color};
 fn main() {
     env_logger::init();
 
-    let b = Bernstein { n: 2, i: 0 };
-    let (xpoints, ypoints) = Evaluator::evaluate_r_to_r3(&b, &0f64, &1f64, &100);
-    let (xvalues, yvalues, _zvalues) = Evaluator::split_coords(0, &xpoints, 0, &ypoints, 0, &xpoints);
-
     let mut fg = Figure::new();
-    fg.axes2d().lines(&xvalues, &yvalues, &[Caption("A line"), Color("black")]);
+    let mut axes2d = fg.axes2d();
+    let deg = 2;
+    for i in 0..(deg + 1) {
+        let b = Bernstein { n: deg, i: i };
+        let (xpoints, ypoints) = Evaluator::evaluate_r_to_r3(&b, &0f64, &1f64, &100);
+        let (xvalues, yvalues, _zvalues) = Evaluator::split_coords(0, &xpoints, 0, &ypoints, 0, &xpoints);
+        let caption = format!("B_{}^{}", i, 2);
+        let mut color;
+        if i == 0 { color = Color("black") }
+        else if i == 1 { color = Color("yellow") }
+        else { color = Color("red") }
+        axes2d.lines(&xvalues, &yvalues, &[Caption(&caption), color]);
+    }
+
     fg.show();
 }
